a35e93023e5f71ce31d8d4c58dc48b71cc6130dd
Pierre Bürki <pierre.burki@he-arc.ch>
2019-08-29 16:25:23 +0200

    Implement router
    Co-authored-by: Jeanneret Loïck <lock.jeanneret@gmail.com>

--- dashboard/src/controllers/controller.js
+++ dashboard/src/controllers/controller.js
@@ -1,34 +1,12 @@
 window.Discotron.Controller = class {
-	/**
-	 * Parse URL and call the right specificController.loadPage()
-	 */
-	static route() {
-		
-	}
-
-	/**
-	 * Find which controller is to be called
-	 */
-	static _parseURL(url) {
-		// Check if on correct page and split # 
-	}
-
-	static _addEvents() {
-		// Intercept URL change
-		// Detect if it's still our domain and /dashboard
-		// if previous url is not the same, push to history
-		// route()
-	}
 
 	/**
 	 * Load specified html page
 	 * @param {string} pageName Name of the page
 	 */
-	static loadPage(pageName) {
+	static _loadPage(pageName) {
 		// Get html file and put it into <main>
+		const baseURL = "/dashboard/src/views/";
+		Discotron.utils.load(baseURL + pageName, document.querySelector("main"));
 	}
-};
-
-window.Discotron.Controller._controllers = {
-	//pluginList: window.Discotron.PluginListController
 };
--- dashboard/src/router.js
+++ dashboard/src/router.js
@@ -1,29 +1,65 @@
-class Router {
+window.Discotron.Router = class {
+
     /**
-     * 
-     * @param {*} controllers 
+     * Parse URL and call the right new specificController(...)
+     * @param {string} url URL to route to
      */
-    constructor(controllers) {
-        this.controllers = controllers; // associative table name => controller
-        this._botStatusController = new window.Discotron.BotStatusController();
+    static route(url) {
+        let parsedURL = Discotron.Router._parseURL(url);
+
+        const controllers = {
+            "bot-status": Discotron.BotStatusController,
+            "plugin-list": Discotron.PluginListController,
+            "repository-list": Discotron.RepositoryListController,
+            "guild-settings": Discotron.GuildSettingsController
+        };
+
+        let Controller = controllers[parsedURL.page];
+        if (Controller !== undefined) {
+            new Controller(parsedURL.args);
+        }
     }
 
-    onLinkClick() {
+    /**
+     * Find which controller is to be called
+     * @returns {object}
+     */
+    static _parseURL(url) {
+        // Check if on correct page and split # 
+        // <url>/dashboard#truc?machin=true
+        let urlRegex = /dashboard\/?#?([a-z\-]*)\??(.*)/;
+        let match = urlRegex.exec(url);
 
+        if (match === null) {
+            return false;
+        }
+        let args = {};
+
+        if (match[2] !== "") {
+            let couples = match[2].split("&");
+
+            for (let i = 0; i < couples.length; i++) {
+                let split = couples[i].split("=");
+                if (split.length === 2) {
+                    args[split[0]] = split[1];
+                }
+            }
+        }
+
+        return {
+            page: match[1],
+            args: args
+        };
     }
 
-    onURLChange() {
-        
+    static _onUrlChange(event) {
+        Discotron.Router.route(event.newURL);
+        // Detect if it's still our domain and /dashboard
+        // if previous url is not the same, push to history
+        // route()
     }
 
-    loadPage() {
-
+    static addEvents() {
+        window.addEventListener("hashchange", Discotron.Router._onUrlChange);
     }
-
-    registerEventsListener() {
-    }
-    
-    parseURL(url) {
-        
-    }
-}
+};
