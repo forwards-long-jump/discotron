e74091e41b3623e9fea6f7de4095e808d9652104
Blatoy <loick.jeanneret@he-arc.ch>
2019-09-02 00:25:48 +0200

    Add login and claiming ownership

--- dashboard/src/controllers/login-controller.js
+++ dashboard/src/controllers/login-controller.js
@@ -1,19 +1,31 @@
 window.Discotron.LoginController = class /* does not extends Controller because it is not a logged in page*/ {
     /**
-     * Call API to get an app token, saves the app token in localStorage
-     * @param {string} authToken Discord given authentification token
+     * Login and claim ownership
      */
-    static login(authToken) {
-        // Cannot login if no owners, claimOwnership is used instead
-    }
+    static claimOwnership() {
+        let url = new URL(window.location.href);
+        let code = url.searchParams.get("code");
+        let ownerSecret = document.querySelector("#owner-ship-token").value;
 
-    /**
-     * Replaces standard login as long as the bot has no owner
-     * Logins, and sets the current user as owner
-     * @param {string} authToken Discord given authentification token
-     * @param {string} secret Secret given to the user in the console     
-     * */
-    static claimOwnership(authToken, secret) {
+        console.log(code, ownerSecret);
+
+        Discotron.WebAPI.queryBot("discotron-dashboard", "claim-ownership", {
+            "code": code,
+            "ownerSecret": ownerSecret
+        }).then((data) => {
+            console.log(data);
+
+            switch (data.status) {
+                case "error":
+                    Discotron.LoginController._displayContainer("claim-ownership");
+                    document.querySelector("#claim-error").style.display = "block";
+                    break;
+                case "success":
+                    localStorage.setItem("appToken", data.token);
+                    window.location.replace("/dashboard");
+                    break;
+            }
+        });
 
     }
 
@@ -27,16 +39,43 @@ window.Discotron.LoginController = class /* does not extends Controller because 
     /** 
      * Set URL from config and check what to display
      */
-    static setupUI() {
+    static initPage() {
         document.querySelector("#auth-link").href = Discotron.config.oauthURL;
+
         let url = new URL(window.location.href);
         let code = url.searchParams.get("code");
-        console.log(code);
-        
-        if(code !== null) {
-            document.querySelector("#logging-in").style.display = "block";
-            document.querySelector("#login").style.display = "none";
+
+        // Code is set, user is trying to log in
+        if (code !== null) {
+            Discotron.LoginController._displayContainer("logging-in");
+
+            Discotron.WebAPI.queryBot("discotron-dashboard", "login", {
+                "code": code
+            }).then((data) => {
+                console.log(data);
+
+                switch (data.status) {
+                    case "error":
+                        Discotron.LoginController._displayContainer("login");
+                        document.querySelector("#login-error").style.display = "block";
+                        break;
+                    case "success":
+                        localStorage.setItem("appToken", data.token);
+                        window.location.replace("/dashboard");
+                        break;
+                    case "first-launch":
+                        Discotron.LoginController._displayContainer("claim-ownership");
+                        break;
+                }
+            });
+
         }
+    }
+
+    static _displayContainer(containerName) {
+        document.querySelector("#logging-in").style.display = containerName === "logging-in" ? "block" : "none";
+        document.querySelector("#login").style.display = containerName === "login" ? "block" : "none";
+        document.querySelector("#claim-ownership").style.display = containerName === "claim-ownership" ? "block" : "none";
     }
 
     /**
@@ -48,5 +87,5 @@ window.Discotron.LoginController = class /* does not extends Controller because 
 };
 
 Discotron.LoginController.handleOwnershipClaim();
-Discotron.LoginController.setupUI();
+Discotron.LoginController.initPage();
 Discotron.LoginController.addEvents();
