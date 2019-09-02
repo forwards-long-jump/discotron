397f2927bcb895038fc9552dc8578cbd4e90a6bd
Blatoy <loick.jeanneret@he-arc.ch>
2019-09-02 11:45:30 +0200

    Remove debug

--- dashboard/src/controllers/navigation-controller.js
+++ dashboard/src/controllers/navigation-controller.js
@@ -17,11 +17,7 @@ window.Discotron.NavigationController = class {
 	 * Load bot status and display it on top
 	 */
 	static displayBot() {
-		console.log("lel");
-		
 		Discotron.WebAPI.queryBot("discotron-dashboard", "get-bot-info").then((data) => {
-			console.log("Queri");
-			
 			document.querySelector("#bot-info img").src = data.avatar;
 			document.querySelector("#bot-info span").textContent = data.username;
 		});
