88ae01d508f83091919ce1b0e2d6f51978d88beb
Pierre Bürki <pierre.burki@he-arc.ch>
2019-09-02 14:11:32 +0200

    Implement permission in bot models

--- bot/classes/permission.js
+++ bot/classes/permission.js
@@ -1,13 +1,17 @@
 const PermissionModel = require("./../../models/permission.js");
 
+const Plugin = require("./plugin.js");
+const Guild = require("./guild.js");
+
 class Permission extends PermissionModel {
     /**
      * Ctor
+     * @param {string} guildId Discord ID of the guild
+     * @param {string} pluginId ID of the plugin
      * @param {array} userRoles Array of user roles allowed to use 
-     * @param {string} pluginId 
      */
-    constructor(userRoles, pluginId) {
-        super(userRoles, pluginId);
+    constructor(guildId, pluginId, userRoles) {
+        super(guildId, pluginId, userRoles);
     }
 
     /**
@@ -15,6 +19,14 @@ class Permission extends PermissionModel {
      * @param {string} discordUserId
      */
     allows(discordUserId) {
+        if (this.usersRoles.length === 0) {
+            if (Plugin.getAll()[this.pluginId].defaultPermission === "everyone") {
+                return true;
+            }
+            // Else it's admins only
+            return Guild.isGuildAdmin(discordUserId, this.guildId);
+        }
+        
         for (let i = 0; i < this.usersRoles.length; ++i) {
             if (this.usersRoles[i].describes(discordUserId)) {
                 return true;
