7d2d321c56b362bf4add95b7439f456220c68d4a
Blatoy <loick.jeanneret@he-arc.ch>
2019-09-03 16:42:27 +0200

    Add webpages

--- bot/webserver.js
+++ bot/webserver.js
@@ -16,18 +16,23 @@ module.exports.serveDashboard = () => {
     });
 };
 
+module.exports.serveRepositoryFolder = (folderName, repositoryFolderName) => {
+    if (["dashboard", "login", "models"].includes(folderName)) {
+        Logger.log("Could not serve folder **" + folderName + "** because it is a reserved page name.", "warn");
+        return;
+    } else {
+        app.use("/" + folderName, express.static(__dirname + "/repositories/" + repositoryFolderName + "/pages/" + folderName));
+    }
+};
+
 module.exports.startAPIServer = () => {
     app.use(express.json());
     app.post("/api", webAPI.onPost);
 };
 
-app.listen(config.webServer.port, () => { 
+app.listen(config.webServer.port, () => {
     Logger.log("Started Webserver on port **" + config.webServer.port + "**", "info");
 }).on("error", (error) => {
     Logger.log("Could not start webserver on port **" + config.webServer.port + "**", "err");
     Logger.log(error, "err");
 });
-
-
-
-
