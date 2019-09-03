5ee47a2ff509ef26e1c299630b4098f5dab8f00e
Blatoy <loick.jeanneret@he-arc.ch>
2019-09-03 18:06:37 +0200

    Add plugin list for guild on dashboard

--- dashboard/src/controllers/plugin-list-controller.js
+++ dashboard/src/controllers/plugin-list-controller.js
@@ -1,36 +1,64 @@
 window.Discotron.PluginListController = class extends window.Discotron.Controller {
-    /**
-     * Ctor
-     * @param {object} args Args given by the user in the URL
-     */
+	/**
+	 * Ctor
+	 * @param {object} args Args given by the user in the URL
+	 */
 	constructor(args) {
-		super("admin/plugin-list.html");
-		this._guild = args._guild;
-		if (this._guild === undefined) {
-			//TODO: redirect
-		}
+		super("admin/plugin-list.html", () => {
+			this._guildId = args.guild;
+
+			if (this._guildId === undefined) {
+				window.location.replace("/dashboard");
+				return;
+			}
+
+			Discotron.Guild.getAll().then((guilds) => {
+				this._guild = guilds[this._guildId];
+				if (this._guild === undefined) {
+					window.location.replace("/dashboard");
+				}
+
+				this._displayHeader();
+				this._displayPlugins();
+			});
+		});
 	}
 
 	/**
-	 * Add a plugin card, containing a checkbox for enabling, and a permission setting
+	 * Displays the image of the guild and its name
+	 */
+	_displayHeader() {
+		document.getElementById("header-icon").src = this._guild.iconURL;
+		document.getElementById("guild-name").textContent = this._guild.name;
+		document.getElementById("go-back").href += this._guild.discordId;
+	}
+
+	/**
+	 * Display plugins
 	 * @param {Plugin} plugin Plugin
 	 */
-	_displayPluginCard(plugin) {
+	_displayPlugins() {
+		// Query cards
+		Discotron.Plugin.getAll().then((plugins) => {
 
+			for (let pluginId in plugins) {
+				const plugin = plugins[pluginId];
+
+				let template = document.getElementById("template-card");
+				let card = document.importNode(template.content, true);
+
+				card.querySelector(".repository-card-title").textContent = plugin.name;
+				card.querySelector(".repository-card-description").textContent = plugin.description;
+
+				card.querySelector(".repository-card").onclick = () => {
+					// TODO: Display UserRoleSelector with custom header
+					/*new Discotron.PluginSettingsWidgetController(plugin, (settings) => {
+						// TOOD: Save this to db
+					});*/
+				};
+
+				document.getElementById("plugin-container").appendChild(card);
+			}
+		});
 	}
-
-	/**
-	 * Display user/role selector
-	 */
-	_onPluginPermissionClick() {
-
-	}
-
-	/**
-	 * Handle plugin enabling
-	 */
-	_onPluginEnabledClick() {
-
-	}
-
 };
