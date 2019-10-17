1d067260b4e15a6ddcbbef259d6d0213c4bcd3ae
Blatoy <lock.jeanneret@gmail.com>
2019-10-17 02:46:44 +0200

    Fix tag/discriminator confusion

--- dashboard/src/models/user.js
+++ dashboard/src/models/user.js
@@ -7,12 +7,13 @@ window.Discotron.User = class {
      * @param {string} name Name of the user
      * @param {string} id Id of the user
      * @param {string} avatarURL Avatar of the user
-     * @param {string} tag Called tag, should be discriminator. FIXEME!
+     * @param {string} discriminator Discriminator of the user (numbers after the #)
      */
-    constructor(name, id, avatarURL, tag) {
+    constructor(name, id, avatarURL, discriminator) {
         this._name = name;
-        this._tag = tag;
+        this._discriminator = discriminator;
         this._id = id;
+        this._tag = name + "#" + discriminator;
         this._avatarURL = avatarURL;
 
         Discotron.User._users[id] = this;
@@ -26,7 +27,14 @@ window.Discotron.User = class {
     }
 
     /**
-     * @returns {string} returns the DISCRIMINATOR
+     * @returns {string} returns the discriminator
+     */
+    get discriminator() {
+        return this._discriminator;
+    }
+
+    /**
+     * @returns {string} returns the tag
      */
     get tag() {
         return this._tag;
@@ -56,7 +64,7 @@ window.Discotron.User = class {
         return new Promise((resolve, reject) => {
             Discotron.WebAPI.queryBot("discotron-dashboard", "get-members", {}, discordGuildId).then((users) => {
                 resolve(users.map((user) => {
-                    return new Discotron.User(user.name, user.id, user.avatar, user.name + "#" + user.discriminator).id;
+                    return new Discotron.User(user.name, user.id, user.avatar, user.discriminator).id;
                 }));
             }).catch(reject);
         });
@@ -74,8 +82,8 @@ window.Discotron.User = class {
                 // Since the user should be loaded if he was in a guild, we are here trying to fetch an out-of-guild user (typically for the owner)
                 Discotron.WebAPI.queryBot("discotron-dashboard", "get-user-info", {
                     discordId: id
-                }).then((userObj) => {
-                    resolve(new Discotron.User(userObj.name, userObj.id, userObj.avatarURL, userObj.tag));
+                }).then((userObject) => {
+                    resolve(new Discotron.User(userObject.name, userObject.id, userObject.avatarURL, userObject.discriminator));
                 }).catch(reject);
             } else {
                 resolve(Discotron.User._users[id]);
