a90b85f48317fadae1692fc57b0fa93c6c739832
RedMser <redmser.jj2@gmail.com>
2020-02-19 16:11:26 +0100

    Add custom no-warning-comments-case eslint rule (and fix issues caused by adding the rule). Maybe this plug-in will later be renamed and extended to include more custom rules.

--- install.js
+++ install.js
@@ -74,7 +74,7 @@ do {
 
         console.log("Invalid value! Must be a domain name (http(s)://) or an IP address.");
     }
-    // todo we could auto-generate this url, but user still has to be prompted to specify on the app's page!
+    // TODO: We could auto-generate this url, but user still has to be prompted to specify on the app's page!
     console.log("(OAuth2 tab) On the tab, for the Redirection URL, enter", domain);
     redirectURL = readlineSync.question("             Select scopes 'identify' and 'guilds' and copy the generated URL: ");
 
