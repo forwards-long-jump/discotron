1659d5ad6b3cd2c27459ca155d0e7dcb04175758
RedMser <redmser.jj2@gmail.com>
2020-03-14 14:01:23 +0100

    Turn plugin ctor args into object.

--- shared-models/plugin.js
+++ shared-models/plugin.js
@@ -4,17 +4,18 @@
 class PluginModel {
     /**
      * @class
-     * @param {string} id Id of the plugin
-     * @param {string} name Displayed name of the plugin
-     * @param {string} description Short description of the plugin
-     * @param {string} version Version of the plugin (format x.y.z)
-     * @param {string} prefix Prefix set by the owner for all servers
-     * @param {object} commands Object containing arrays of Command objects, grouped by trigger type
-     * @param {string} defaultPermission Who can access the command if no permissions are set by the server owner, can be *everyone*, *admin*
-     * @param {boolean} enabled True if plugin is enabled by the bot owner
-     * @param {Array} logs List of logs the plugin can output to
+     * @param {object} options Args
+     * @param {string} options.id Id of the plugin
+     * @param {string} options.name Displayed name of the plugin
+     * @param {string} options.description Short description of the plugin
+     * @param {string} options.version Version of the plugin (format x.y.z)
+     * @param {string} options.prefix Prefix set by the owner for all servers
+     * @param {object} options.commands Object containing arrays of Command objects, grouped by trigger type
+     * @param {string} options.defaultPermission Who can access the command if no permissions are set by the server owner, can be *everyone*, *admin*
+     * @param {boolean} options.enabled True if plugin is enabled by the bot owner
+     * @param {Array} options.logs List of logs the plugin can output to
      */
-    constructor(id = "", name = "", description = "", version = "", prefix = "", commands = {}, defaultPermission = "everyone", enabled = true, logs = []) {
+    constructor({id = "", name = "", description = "", version = "", prefix = "", commands = {}, defaultPermission = "everyone", enabled = true, logs = []}) {
         this._name = name;
         this._id = id;
         this._description = description;
@@ -27,62 +28,62 @@ class PluginModel {
     }
 
     /**
-     * @returns {string} Name
+     * @returns {string} Displayed name of the plugin
      */
     get name() {
         return this._name;
     }
     /**
-     * @returns {string} Id
+     * @returns {string} Id of the plugin
      */
     get id() {
         return this._id;
     }
 
     /**
-     * @returns {string} Description
+     * @returns {string} Short description of the plugin
      */
     get description() {
         return this._description;
     }
 
     /**
-     * @returns {string} Version
+     * @returns {string} Version of the plugin (format x.y.z)
      */
     get version() {
         return this._version;
     }
 
     /**
-     * @returns {string} Prefix
+     * @returns {string} Prefix set by the owner for all servers
      */
     get prefix() {
         return this._prefix;
     }
 
     /**
-     * @returns {Array} Array of Command 
+     * @returns {object} Object containing arrays of Command objects, grouped by trigger type
      */
     get commands() {
         return this._commands;
     }
 
     /**
-     * @returns {string} Default permission (*everyone* or *admin*)
+     * @returns {string} Who can access the command if no permissions are set by the server owner, can be *everyone*, *admin*
      */
     get defaultPermission() {
         return this._defaultPermission;
     }
 
     /**
-     * @returns {boolean} Enabled
+     * @returns {boolean} True if plugin is enabled by the bot owner
      */
     get enabled() {
         return this._enabled;
     }
 
     /**
-     * @returns {Array} Array of string
+     * @returns {Array} List of logs the plugin can output to
      */
     get logs() {
         return this._logs;
